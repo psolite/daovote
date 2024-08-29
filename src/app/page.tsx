e42d678ceda41959b31d7182fa94e1593d086cd5
Chinwuba Israel <chiizzy5@gmail.com>
2024-08-29 19:56:43 +0100

    style component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,10 +11,10 @@ export default function Home() {
     <main className="min-h-screen">
       {/* <Hero /> */}
       {/* <CreatePoll /> */}
-      {/* <AllPolls /> */}
+      <AllPolls />
       {/* <Vote /> */}
       {/* <VoteSummary /> */}
-      <VotedCard />
+      {/* <VotedCard /> */}
     </main>
   );
 }
