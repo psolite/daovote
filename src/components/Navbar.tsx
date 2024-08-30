9ecbe227de145180fd325893c85b5d4342b771c1
psolite <info@bulldeveloper.com>
2024-08-30 16:50:32 +0200

    canvas wallet update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,7 +14,7 @@ interface NavbarProps { }
 const Navbar: FC<NavbarProps> = ({ }) => {
   const { publicKey, wallet } = useWallet()
   const { walletAddress, iframe, connectWallet, userInfo, walletIcon } = useCanvasWallet()
-
+  console.log(userInfo?.avatar)
   return (
     <nav className="bg-white fixed top-0 left-0 right-0 z-50">
       <div className="container mx-auto max-w-[1200px] w-full relative">
@@ -25,13 +25,13 @@ const Navbar: FC<NavbarProps> = ({ }) => {
             {iframe ?
               (walletAddress ? <Button>
                 <Image
-                  src={userInfo?.avatar! ?? walletIcon}
+                  src={userInfo?.avatar || walletIcon || ''}
                   alt={'wallet'}
                   height={15}
                   width={15}
                   className="mr-2"
                 />
-                {userInfo?.username}
+                ({userInfo?.username} || {walletAddress.slice(0, 3)}...{walletAddress.slice(-3)})
               </Button>
 
                 :
