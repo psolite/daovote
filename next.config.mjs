b8d12c9b837f33f0239fab3ba72417aa2db7c60e
psolite <info@bulldeveloper.com>
2024-09-01 08:37:41 +0200

    clipboard for canvas

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
                         key: "Content-Security-Policy",
                         value: "connect-src 'self' https://api.dscvr.one https://api1.stg.dscvr.one https://*.helius-rpc.com https://api.devnet.solana.com wss://api.devnet.solana.com/",
                     },
+                    {
+                        key: "Permissions-Policy",
+                        value: "clipboard-read=(self), clipboard-write=(self)",
+                    }
 
                 ],
             },
